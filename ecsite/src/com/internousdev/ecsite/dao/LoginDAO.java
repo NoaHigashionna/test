281d0e6722724080ee45a7408f7583e7fd623b27
NoaHigashionna <reviysk@yahoo.co.jp>
2018-09-27 17:38:31 +0900

    2018-09-27_その3

--- ecsite/src/com/internousdev/ecsite/dao/LoginDAO.java
+++ ecsite/src/com/internousdev/ecsite/dao/LoginDAO.java
@@ -24,8 +24,8 @@ public class LoginDAO {
 
 			if(rs.next()){
 				loginDTO.setLoginId(rs.getString("login_id"));
-				loginDTO.setLoginId(rs.getString("login_pass"));
-				loginDTO.setLoginId(rs.getString("user_name"));
+				loginDTO.setLoginPassword(rs.getString("login_pass"));
+				loginDTO.setUserName(rs.getString("user_name"));
 
 				if(!(rs.getString("login_id").equals(null))){
 					loginDTO.setLoginFlg(true);
